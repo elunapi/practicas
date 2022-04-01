f18f802c6d5ed343d92627fefa4a4d1fd4fe7922
Erick Luna <elunapi@ciencias.unam.mx>
2022-03-31 20:33:29 -0600

    Se solucionó un problema de Maven haciendo una instalación limpia

--- src/main/java/com/example/practica1/controlador/Controlador.java
+++ src/main/java/com/example/practica1/controlador/Controlador.java
@@ -1,5 +1,26 @@
 package com.example.practica1.controlador;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
 public class Controlador {
-    
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @PostMapping("/tunombre")
+    public String tunombre(HttpServletRequest request, Model model ) {
+        String nombre = request.getParameter("nombre");
+        System.out.println("nombre -> " + nombre);
+        model.addAttribute("nombre", nombre);
+        return "nombres";
+    }
+
 }
